e53d85601b93159f86fa8fcc735dd403be781a23
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-19 21:59:37 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,18 +2,9 @@
   i = 0
  
   while i < array.length
-    yield 
-    array[i]
-    i = i + 1
+    yiel
   hello_t( ["Tim", "Tom", "Jim"] ) do |name|
   if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
-end
-
-def hello_t( ["Tim", "Tom", "Jim"] ) do |name|
-  if name.start_with?( "T" )
-    puts "Hi, #{name}"
+    puts "Hi, #{name}."
   end
 end
