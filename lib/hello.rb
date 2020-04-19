cfa804c71c556491e142528df35883df1c7ce643
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-19 22:06:39 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,2 @@
- def hello_t(array)
-def hello_t( ["Tim", "Tom", "Jim"] ) do |name|
-  if name.start_with?("T")
-    i = 0
- 
-  while i < array.length
-    yield array[i]
-    i = i + 1
-    puts "Hi, #{name}."
-  end
-end
+
 
